d7a59f8cdac3435b30729914005ed7edac10a5ee
hiroyay-ms <hiroyay@microsoft.com>
2022-11-20 22:03:01 +0900

    Update app

--- src/CS/Controllers/HomeController.cs
+++ src/CS/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using We.Models;
+using Web.Models;
 
-namespace We.Controllers;
+namespace Web.Controllers;
 
 public class HomeController : Controller
 {
